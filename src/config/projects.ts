c95a0ce59214e4ae7a6cdbd6a17124bd46e1b387
carreypro <nafiuriak@gmail.com>
2024-12-27 16:55:17 +0800

    feat: 更新项目描述和开源项目配置
    1. 更新项目总体描述文案 2. 修正开源项目的仓库链接 3. 更新项目描述和统计数据

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,6 +1,6 @@
 // projects
 export const projectHeadLine = "What I've done and what I'm doing."
-export const projectIntro = "I've worked on a variety of projects, from simple websites to complex web applications. And many of them are open-source. Here are a few of my favorites."
+export const projectIntro = "I have worked on various projects, ranging from simple websites to complex web applications, games, and more. Here are a few of my favorite ones:"
 
 export type ProjectItemType = {
     name: string
@@ -17,17 +17,38 @@ export type ProjectItemType = {
   }
   
 // projects 
-export const projects: Array<ProjectItemType> = []
+export const projects: Array<ProjectItemType> = [
+  {
+    name: '永劫无间系列',
+    description: '24 Entertainment工作室开发的多人动作竞技游戏',
+    link: { href: 'https://www.yjwujian.cn/index.html', label: '永劫无间' },
+    tags: ['官网', '游戏'],
+  },
+  {
+    name: '猫修APP',
+    description: '游戏社区',
+    link: { href: 'https://www.lmaoyx.com/app', label: '猫修APP' },
+    tags: ['APP', '游戏社区'],
+    logo: 'maoxiu',
+  },
+  {
+    name: '猫修平台',
+    description: '汉化和修改器平台',
+    link: { href: 'https://www.lmaoyx.com', label: '猫修平台' },
+    tags: ['汉化', '修改器', '游戏'],
+    logo: 'maoxiu',
+  }
+]
 
 // github projects
 export const githubProjects: Array<ProjectItemType> = [
   {
     name: '个人网站',
-    description: '临摹项目',
-    link: { href: 'https://github.com/carreypro/portfolio', label: '个人网站' },
+    description: '基于 Next.js 14 开发的个人网站',
+    link: { href: 'github.com/carreypro/carreyproone', label: '个人网站' },
     logo: 'github',
-    gitStars: 1,
-    gitForks: 1,
+    gitStars: 0,
+    gitForks: 0,
     techStack: ['Next.js', 'TailwindCSS', 'TypeScript'],
   },
 ]
