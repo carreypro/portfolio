fe77775335d4a2347234519fa61cd6729445c411
carreypro <nafiuriak@gmail.com>
2024-12-28 23:07:44 +0800

    feat: add new tweet

--- src/config/activity.ts
+++ src/config/activity.ts
@@ -3,6 +3,7 @@ export const activityHeadLine = "My Activity"
 export const activityIntro = "Snippets of My Coding & Life"
 
 export const tweetIds = [
+  '1873022432333971902',
   '1872208318363787476',
   '1871125849019424839'
 ]
