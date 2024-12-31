556778dc088f1e8510f9ee9104588df3d7c9fbed
carreypro <nafiuriak@gmail.com>
2024-12-31 10:54:21 +0800

    feat: 添加新的推文 ID / feat: add new tweet ID

--- src/config/activity.ts
+++ src/config/activity.ts
@@ -3,6 +3,7 @@ export const activityHeadLine = "My Activity"
 export const activityIntro = "Snippets of My Coding & Life"
 
 export const tweetIds = [
+  '1873763936803463664',
   '1873571047141953638',
   '1873022432333971902',
   '1872208318363787476'
